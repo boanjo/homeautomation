82d2a0f932f21716c9ec7febf0eb2808ead838c3
epkboan <epkboan@gmail.com>
2014-01-13 13:08:45 +0100

    Initial app

---
+++ src/homeautomation_app.erl
@@ -0,0 +1,16 @@
+-module(homeautomation_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    homeautomation_sup:start_link().
+
+stop(_State) ->
+    ok.
